26b15ca3f6236950e7acfff395388a7974cce217
Abdoul Karim <abdoul625karim@gmail.com>
2025-07-09 12:13:04 +0000

    Suppression de la possibilité de choisir les caractères et de la taille du mot de passe

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
+let password = "";
+let information = document.querySelector('#information');
 document.querySelector("button").addEventListener("click", function () {
-    let majusculeCheck = document.querySelector('#majuscule');
-    let minisculeCheck = document.querySelector('#minuscule');
-    let numberCheck = document.querySelector('#number');
+    password = "";
     let longueur = document.querySelector('#longueur');
     let p = document.querySelector('p');
-    let passwordLongueur = parseInt(longueur.value);
+    let copie = document.querySelector('#copie');
     const number = "123456789";
     const majuscule = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
-    const miniscule = "abcdefghijklmnopqrstuvwxyz";
-    let character = "";
-    let password = "";
-    if (majusculeCheck.checked) {
-        character += majuscule;
-    }
-    if (minisculeCheck.checked) {
-        character += miniscule;
-    }
-    if (numberCheck.checked) {
-        character += number;
-    }
-    let erreur = document.querySelector('#erreur');
+    const minuscule = "abcdefghijklmnopqrstuvwxyz";
+    let character = number + majuscule + minuscule;
     if (character === "") {
-        erreur.innerHTML = "Veuillez selection un type de caractère";
-        erreur.style.color = "red";
+        information.innerHTML = "Veuillez selection un type de caractère";
+        information.style.color = "red";
     } else {
-        erreur.innerHTML = "";
-        for (let i = 0; i < passwordLongueur; i++) {
+        information.innerHTML = "";
+        for (let i = 0; i < 10; i++) {
             const randomNumber = Math.random();
             let generatedNumber = Math.floor(randomNumber * character.length);
             let generatedCharacter = character[generatedNumber];
             password += generatedCharacter;
         }
         p.textContent = password;
+    }
+});
+
+document.querySelector("#copie").addEventListener("click", function () {
+    if (password === "") {
+        information.innerHTML = "Veuillez générer un mot de passe d'abord";
+        information.style.color = "red";
+    } else {
+        information.style.color = ""
+        navigator.clipboard.writeText(password).then(r => information.innerHTML = "Texte copié");
     }
 });
